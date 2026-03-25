7220e4d876210032d4123d49ecadf8fd5a00d3b8
846c/crxcket <84820661+crcket@users.noreply.github.com>
2026-03-24 20:59:54 -0400

    Update LoadingScreenAI.lua

--- Background/LoadingScreenAI.lua
+++ Background/LoadingScreenAI.lua
@@ -2,17 +2,11 @@ repeat task.wait() until game:IsLoaded()
 
 local RunService = game:GetService("RunService")
 local TweenService = game:GetService("TweenService")
-local CoreGui = game:GetService("CoreGui")
 
--- GUI Setup
-local LoadingScreen = Instance.new("ScreenGui")
-LoadingScreen.IgnoreGuiInset = true
-LoadingScreen.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
-LoadingScreen.ResetOnSpawn = false
-LoadingScreen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-LoadingScreen.Name = "LoadingScreen"
-LoadingScreen.Parent = CoreGui
+local LoadingScreen = Instance.new("ScreenGui",game.CoreGui)
+LoadingScreen.IgnoreGuiInset=true
 
+-- Background
 local Background = Instance.new("Frame")
 Background.AnchorPoint = Vector2.new(0.5, 0.5)
 Background.BackgroundColor3 = Color3.fromRGB(26, 31, 46)
@@ -27,8 +21,7 @@ Box.AnchorPoint = Vector2.new(0.5, 0.5)
 Box.BackgroundColor3 = Color3.fromRGB(27, 34, 53)
 Box.BorderSizePixel = 0
 Box.Position = UDim2.new(0.5, 0, 0.5, 0)
-Box.Size = UDim2.new(0, 250, 0, 200)
-Box.Name = "Box"
+Box.Size = UDim2.new(0, 400, 0, 200)
 Box.Parent = Background
 
 Instance.new("UICorner").Parent = Box
@@ -43,7 +36,7 @@ BoxStroke.Parent = Box
 local TopBar = Instance.new("Frame")
 TopBar.BackgroundColor3 = Color3.fromRGB(15, 21, 32)
 TopBar.BorderSizePixel = 0
-TopBar.Size = UDim2.new(0, 250, 0, 35)
+TopBar.Size = UDim2.new(0, 400, 0, 35)
 TopBar.Parent = Box
 
 local TopBarStroke = Instance.new("UIStroke")
@@ -65,33 +58,33 @@ TopBarLabel.Position = UDim2.new(0.05, 0, 0.5, 0)
 TopBarLabel.Size = UDim2.new(0, 120, 0, 35)
 TopBarLabel.Parent = TopBar
 
--- Active Display
-local ActiveDisplay = Instance.new("Frame")
-ActiveDisplay.AnchorPoint = Vector2.new(0.5, 0)
-ActiveDisplay.BackgroundColor3 = Color3.fromRGB(14, 43, 24)
-ActiveDisplay.BorderSizePixel = 0
-ActiveDisplay.Position = UDim2.new(0.5, 0, 0.25, 0)
-ActiveDisplay.Size = UDim2.new(0, 220, 0, 35)
-ActiveDisplay.Parent = Box
+-- Active Frame
+local ActiveFrame = Instance.new("Frame")
+ActiveFrame.AnchorPoint = Vector2.new(0.5, 0)
+ActiveFrame.BackgroundColor3 = Color3.fromRGB(14, 43, 24)
+ActiveFrame.BorderSizePixel = 0
+ActiveFrame.Position = UDim2.new(0.5, 0, 0.25, 0)
+ActiveFrame.Size = UDim2.new(0, 380, 0, 35)
+ActiveFrame.Parent = Box
 
-local ActiveDisplayCorner = Instance.new("UICorner")
-ActiveDisplayCorner.CornerRadius = UDim.new(0, 5)
-ActiveDisplayCorner.Parent = ActiveDisplay
+local ActiveFrameCorner = Instance.new("UICorner")
+ActiveFrameCorner.CornerRadius = UDim.new(0, 5)
+ActiveFrameCorner.Parent = ActiveFrame
 
-local ActiveDisplayStroke = Instance.new("UIStroke")
-ActiveDisplayStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-ActiveDisplayStroke.Color = Color3.fromRGB(26, 92, 46)
-ActiveDisplayStroke.Thickness = 0.85
-ActiveDisplayStroke.Parent = ActiveDisplay
+local ActiveFrameStroke = Instance.new("UIStroke")
+ActiveFrameStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+ActiveFrameStroke.Color = Color3.fromRGB(26, 92, 46)
+ActiveFrameStroke.Thickness = 0.85
+ActiveFrameStroke.Parent = ActiveFrame
 
 local Dot = Instance.new("Frame")
 Dot.AnchorPoint = Vector2.new(0.5, 0.5)
 Dot.BackgroundColor3 = Color3.fromRGB(0, 179, 65)
 Dot.BackgroundTransparency = 0.5
 Dot.BorderSizePixel = 0
-Dot.Position = UDim2.new(0.204545453, 0, 0.5, 0)
+Dot.Position = UDim2.new(0.262, 0, 0.5, 0)
 Dot.Size = UDim2.new(0, 8, 0, 8)
-Dot.Parent = ActiveDisplay
+Dot.Parent = ActiveFrame
 
 local DotCorner = Instance.new("UICorner")
 DotCorner.CornerRadius = UDim.new(1, 0)
@@ -99,30 +92,30 @@ DotCorner.Parent = Dot
 
 local ActiveLabel = Instance.new("TextLabel")
 ActiveLabel.Font = Enum.Font.GothamBold
-ActiveLabel.Text = "AUTOFARM ACTIVE"
+ActiveLabel.Text = "AUTOFARM ACTIVE - 999s"
 ActiveLabel.TextColor3 = Color3.fromRGB(0, 179, 65)
 ActiveLabel.TextSize = 14
 ActiveLabel.TextXAlignment = Enum.TextXAlignment.Right
 ActiveLabel.AnchorPoint = Vector2.new(0, 0.5)
 ActiveLabel.BackgroundTransparency = 1
 ActiveLabel.BorderSizePixel = 0
-ActiveLabel.Position = UDim2.new(1.6, 0, 0.5, 0)
+ActiveLabel.Position = UDim2.new(6.6, 0, 0.5, 0)
 ActiveLabel.Size = UDim2.new(0, 120, 0, 35)
 ActiveLabel.Parent = Dot
 
 -- HP Labels
-local HPLabel = Instance.new("TextLabel")
-HPLabel.Font = Enum.Font.GothamBold
-HPLabel.Text = "BOSS HP"
-HPLabel.TextColor3 = Color3.fromRGB(61, 80, 112)
-HPLabel.TextSize = 13
-HPLabel.TextXAlignment = Enum.TextXAlignment.Left
-HPLabel.TextYAlignment = Enum.TextYAlignment.Bottom
-HPLabel.BackgroundTransparency = 1
-HPLabel.BorderSizePixel = 0
-HPLabel.Position = UDim2.new(0.06, 0, 0.5, 0)
-HPLabel.Size = UDim2.new(0, 200, 0, 15)
-HPLabel.Parent = Box
+local BossHPLabel = Instance.new("TextLabel")
+BossHPLabel.Font = Enum.Font.GothamBold
+BossHPLabel.Text = "BOSS HP"
+BossHPLabel.TextColor3 = Color3.fromRGB(61, 80, 112)
+BossHPLabel.TextSize = 13
+BossHPLabel.TextXAlignment = Enum.TextXAlignment.Left
+BossHPLabel.TextYAlignment = Enum.TextYAlignment.Bottom
+BossHPLabel.BackgroundTransparency = 1
+BossHPLabel.BorderSizePixel = 0
+BossHPLabel.Position = UDim2.new(0.025, 0, 0.5, 0)
+BossHPLabel.Size = UDim2.new(0, 200, 0, 15)
+BossHPLabel.Parent = Box
 
 local Percentage = Instance.new("TextLabel")
 Percentage.Font = Enum.Font.GothamBold
@@ -132,23 +125,23 @@ Percentage.TextScaled = true
 Percentage.TextWrapped = true
 Percentage.BackgroundTransparency = 1
 Percentage.BorderSizePixel = 0
-Percentage.Position = UDim2.new(0.06, 0, 0.575, 0)
-Percentage.Size = UDim2.new(0, 49, 0, 30)
+Percentage.Position = UDim2.new(0.025, 0, 0.575, 0)
+Percentage.Size = UDim2.new(0, 50, 0, 30)
 Percentage.Parent = Box
 
--- HP Bar
-local HPBarBG = Instance.new("CanvasGroup")
-HPBarBG.AnchorPoint = Vector2.new(0.5, 0)
-HPBarBG.BackgroundColor3 = Color3.fromRGB(13, 17, 32)
-HPBarBG.BorderSizePixel = 0
-HPBarBG.Position = UDim2.new(0.5, 0, 0.75, 0)
-HPBarBG.Size = UDim2.new(0, 220, 0, 15)
-HPBarBG.ZIndex = -1
-HPBarBG.Parent = Box
+-- Boss Bar
+local BossBarHolder = Instance.new("CanvasGroup")
+BossBarHolder.AnchorPoint = Vector2.new(0.5, 0)
+BossBarHolder.BackgroundColor3 = Color3.fromRGB(13, 17, 32)
+BossBarHolder.BorderSizePixel = 0
+BossBarHolder.Position = UDim2.new(0.5, 0, 0.75, 0)
+BossBarHolder.Size = UDim2.new(0, 380, 0, 15)
+BossBarHolder.ZIndex = -1
+BossBarHolder.Parent = Box
 
-local HPBarBGCorner = Instance.new("UICorner")
-HPBarBGCorner.CornerRadius = UDim.new(0, 3)
-HPBarBGCorner.Parent = HPBarBG
+local BossBarHolderCorner = Instance.new("UICorner")
+BossBarHolderCorner.CornerRadius = UDim.new(0, 3)
+BossBarHolderCorner.Parent = BossBarHolder
 
 local HPBar = Instance.new("Frame")
 HPBar.AnchorPoint = Vector2.new(0, 0.5)
@@ -156,27 +149,27 @@ HPBar.BackgroundColor3 = Color3.fromRGB(204, 34, 34)
 HPBar.BorderSizePixel = 0
 HPBar.Position = UDim2.new(0, 0, 0.5, 0)
 HPBar.Size = UDim2.new(1, 0, 1, 0)
-HPBar.Parent = HPBarBG
+HPBar.Parent = BossBarHolder
 
 local HPBarCorner = Instance.new("UICorner")
 HPBarCorner.CornerRadius = UDim.new(0, 3)
 HPBarCorner.Parent = HPBar
 
--- Shining Outline
-local ShiningOutline = Instance.new("UIStroke")
-ShiningOutline.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-ShiningOutline.Color = Color3.fromRGB(255, 255, 255)
-ShiningOutline.Thickness = 2
-ShiningOutline.ZIndex = 2
-ShiningOutline.Parent = Box
+-- Outline Shine
+local OutlineShine = Instance.new("UIStroke")
+OutlineShine.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+OutlineShine.Color = Color3.fromRGB(255, 255, 255)
+OutlineShine.Thickness = 2
+OutlineShine.ZIndex = 2
+OutlineShine.Parent = Box
 
-local ShiningGradient = Instance.new("UIGradient")
-ShiningGradient.Color = ColorSequence.new{
+local OutlineGradient = Instance.new("UIGradient")
+OutlineGradient.Color = ColorSequence.new{
 	ColorSequenceKeypoint.new(0, Color3.fromRGB(68, 87, 130)),
 	ColorSequenceKeypoint.new(1, Color3.fromRGB(68, 87, 130))
 }
-ShiningGradient.Rotation = 180
-ShiningGradient.Transparency = NumberSequence.new{
+OutlineGradient.Rotation = 180
+OutlineGradient.Transparency = NumberSequence.new{
 	NumberSequenceKeypoint.new(0, 0.9),
 	NumberSequenceKeypoint.new(0.399, 1),
 	NumberSequenceKeypoint.new(0.4, 0),
@@ -184,17 +177,18 @@ ShiningGradient.Transparency = NumberSequence.new{
 	NumberSequenceKeypoint.new(0.601, 1),
 	NumberSequenceKeypoint.new(1, 0.9)
 }
-ShiningGradient.Parent = ShiningOutline
+OutlineGradient.Parent = OutlineShine
 
 -- Dot Pulse
-local circleTweenInfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.In, -1, true)
-TweenService:Create(Dot, circleTweenInfo, {BackgroundTransparency = 1}):Play()
+local dotTweenInfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.In, -1, true)
+TweenService:Create(Dot, dotTweenInfo, {BackgroundTransparency = 1}):Play()
 
--- Shining Outline Rotation
-local outlineRot = ShiningGradient.Rotation
+-- Outline Rotation
+local outlineRot = OutlineGradient.Rotation
 RunService.Heartbeat:Connect(function(dt)
 	outlineRot = (outlineRot + 72 * dt) % 360
-	ShiningGradient.Rotation = outlineRot
+	OutlineGradient.Rotation = outlineRot
+	ActiveLabel.Text = `AUTOFARM ACTIVE — {math.round(workspace.DistributedGameTime)}s`
 end)
 
 workspace:WaitForChild("Live").ChildAdded:Connect(function(v)
@@ -215,3 +209,5 @@ bossHum:GetPropertyChangedSignal("Health"):Connect(function()
 		Size = UDim2.new(bossHum.Health / bossHum.MaxHealth, 0, 1, 0)
 	}):Play()
 end)
+
+
